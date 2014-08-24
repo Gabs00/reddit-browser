c6f4371b735fb764fbeebda8cd69e69e8b95611b
Gabs00 <gewen87@gmail.com>
2014-08-23 22:27:10 -0400

    got soemthing there

--- web.js
+++ web.js
@@ -2,12 +2,16 @@ var express = require('express');
 var restler = require('restler');
 
 var app = express.createServer(express.logger());
-
-app.all('/', function(request, response) {
+app.get('/', function(req, res, next){
+  fs.readFile('index.html', function(data){
+    res.send(data);
+  });
+});
+app.all('/reddit', function(request, response) {
 	restler.get('http://reddit.com/.json').on('complete', function(reddit) {
 		var titles = "<Response>";
 		for(var i=0; i<5; i++) {
-			titles += "<Sms>" + reddit.data.children[i].data.title + "</Sms>";
+			titles += "<div style='border: 1px solid red'>" + reddit.data.children[i].data.title + "</div>";
 		}
 		titles += "</Response>";
 		response.send(titles);
